45e92fdba170e59bfc8f519fd73f51868c7a07b5
Prasanna_N <prasannanaikpnpn@gmail.com>
2020-11-24 15:54:19 +0530

    again updated for loop

---
+++ forLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i=0; i<7; i++)
+    printf("Hello world!\n");
+    return 0;
+    
+    printf("Done with for loop");
+    printf("What about for loop");
+}
